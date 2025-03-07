0274e98ed8f9a3d895508e87e37f1dfa63208970
joaomalfatti <joao.cursos1210@gmail.com>
2025-03-07 20:31:50 -0300

    Configurando swagger e adicionando request

---
+++ FleetOrganization.API/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using FleetOrganiztion.COMMUNICATION.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetOrganization.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+
+public class UsersController : ControllerBase
+{
+    //endpoint
+    [HttpPost]
+
+    public IActionResult Create(RequestUserJson requestUser)
+    {
+        return Created();
+    }
+
+}
+
